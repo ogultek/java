bd8acfb799c4d6d1cd39944752bece972e63f599
ogultek <71530909+ogultek@users.noreply.github.com>
2020-09-19 00:45:44 +0200

    Assigment1

---
+++ Question7.java
@@ -0,0 +1,17 @@
+package day_2;
+
+public class Question7 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("    /\\  /\\");
+		System.out.println("   /__\\/__\\");
+		
+		System.out.println("I am from \"Germany\".\r\n" + 
+				"\r\n" + 
+				"How is your day going on, 'good' or 'bad' ? ");
+		
+
+	}
+
+}
